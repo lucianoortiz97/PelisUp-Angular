32a65cb983d8c0d88caaf768d9cadc92924e22a4
luchoortiz97 <lucianoortiz99@gmail.com>
2023-10-01 23:46:43 -0300

    Actualizacion modulos y componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule } from './app-routing.module';
+import { LayoutModule } from "./layout/layout.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    NgbModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        NgbModule,
+        AppRoutingModule,
+        LayoutModule
+    ]
 })
 export class AppModule { }
---
+++ src/app/routes/routes.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PeliculasComponent } from './peliculas/peliculas.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { SeriesComponent } from './series/series.component';
+import { IngresarComponent } from './ingresar/ingresar.component';
+
+
+
+@NgModule({
+  declarations: [
+    PeliculasComponent,
+    InicioComponent,
+    SeriesComponent,
+    IngresarComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class RoutesModule { }
